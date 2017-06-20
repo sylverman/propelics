30e3e5e5be23bd56397098fe3f9e5f7edbfaff03
Daniel Vilchis <sylverman.mx@gmail.com>
2017-06-20 09:10:45 -0500

    Adding exercise 4

--- js/exercise4.js
+++ js/exercise4.js
@@ -1,7 +1,23 @@
-// Using prototyping for this exercise
-/*var comparingStrings = function() {
 
-}*/
 
-stringOne = "the red bird is flying";
-stringTwo = "The clouds looks beautiful in the sky";
+stringOne = document.getElementById('exercise4String1');
+stringTwo = document.getElementById('exercise4String2');
+resultBoxExercise4 = document.getElementById('resultExercise4');
+
+stringOne.onkeyup = function(){
+  findingMatches(document.getElementById('exercise4String1').value, document.getElementById('exercise4String2').value);
+}
+stringTwo.onkeyup = function(){
+  findingMatches(document.getElementById('exercise4String1').value, document.getElementById('exercise4String2').value);
+}
+
+function findingMatches(stringOne, stringTwo){
+  var matchedCharacters = stringOne.split("").filter((val, index, arr) => { //ECMA6 Arrow functions
+    if(val != " " && stringTwo.indexOf(val) != -1){
+      return arr.indexOf(val, index+1) < 0
+    }
+  });
+  resultBoxExercise4.innerHTML = matchedCharacters.toString().replace(/[ ]*,[ ]*|[ ]+/g, " ");
+  //return matchedCharacters;
+}
+findingMatches(document.getElementById('exercise4String1').value, document.getElementById('exercise4String2').value);
